aa0921089c3a40064af3cb2e9413d553590ebaae
Brock808 <marcoscattaneo08@gmail.com>
2026-09-21 13:44:00 +0200

    Finished the Personal proyect in branch 2 before merge

---
+++ README.md
@@ -0,0 +1,65 @@
+## 🌱 Learning Goals
+ 
+This project was created to practice and consolidate concepts learned during Front-End Technologies studies.
+ 
+Main focus areas:
+ 
+- Building responsive layouts with Bootstrap
+- Working with CSS Variables and themes
+- DOM manipulation using JavaScript
+- jQuery event handling
+- Bootstrap components integration
+- Version control using Git and GitHub
+ 
+During development, Git branches, commits and merges were used to simulate a real-world development workflow.
+
+## 📚 Skills Practiced
+ 
+### HTML
+- Semantic Structure
+- Forms
+- Images
+- Tables
+- Lists
+ 
+### CSS
+- CSS Variables
+- Flexbox
+- Responsive Styling
+- Hover Effects
+- Dark/Light Mode
+ 
+### Bootstrap
+- Navbar
+- Grid System
+- Carousel
+- Cards
+- Buttons
+- Toasts
+- Tables
+- List Groups
+- Bootstrap Icons
+- 12 Column System, Rows, Cols
+- Flexbox
+- Responsive Columns, Responsive Rows
+ 
+### JavaScript
+- Event Handling
+- DOM Manipulation
+- Random Movie Recommendation
+- Bootstrap Toast Control
+ 
+### jQuery
+- Click Events
+- Dynamic Theme Switching
+- Class Manipulation
+- Interactive Elements
+ 
+### Git & GitHub
+- Repository Management
+- Commits
+- Branch Creation
+- Branch Switching
+- Merge Operations
+- Project Version Control
+- GitHub Repository Publishing
--- js.js
+++ js.js
@@ -1,30 +1,19 @@
 $(function(){
-                $(".addToast").on("click", function(){
+                $(".addToast, #recommendationBanner").on("click", function(){
                     $("#imToast").toast("show")
                 })
 
                 $("#darkMode").on("click", function(){
-                    $("body").addClass("nightMode")
-                    $("p, p.h1, p.h2, p.h3, p.h4, p.h5, p.h6, button").addClass("nightMode")
-                    $("#hud, #footer").addClass("nightMode")
-                    $(".carousel-control-next, .carousel-control-prev").addClass("nightMode")
-                    $(".btnBanner").addClass("nightMode")
-                    $(".card").addClass("nightMode")
-                    $("i").addClass("nightMode")
-                    $(".irregulars").addClass("nightMode")
-                    $(".toast").addClass("nightMode")
-                })
-
-                $("#lightMode").on("click", function(){
-                    $("body").removeClass("nightMode")
-                    $("p, p.h1, p.h2, p.h3, p.h4, p.h5, p.h6, button").removeClass("nightMode")
-                    $("#hud, #footer").removeClass("nightMode")
-                    $(".carousel-control-next, .carousel-control-prev").removeClass("nightMode")
-                    $(".btnBanner").removeClass("nightMode")
-                    $(".card").removeClass("nightMode")
-                    $("i").removeClass("nightMode")
-                    $(".irregulars").removeClass("nightMode")
-                    $(".toast").removeClass("nightMode")
+                    $("body").toggleClass("nightMode")
+                    $("p, p.h1, p.h2, p.h3, p.h4, p.h5, p.h6, button").toggleClass("nightMode")
+                    $("#hud, #footer").toggleClass("nightMode")
+                    $(".carousel-control-next, .carousel-control-prev").toggleClass("nightMode")
+                    $(".btnBanner").toggleClass("nightMode")
+                    $(".card").toggleClass("nightMode")
+                    $("i").toggleClass("nightMode")
+                    $(".iregulars").toggleClass("nightMode")
+                    $(".toast").toggleClass("nightMode")
+                    $(".dropdown-item, .dropdown-menu").toggleClass("nightMode")
                 })
 
                 $("#btnRecomendation").on("click", function(){
